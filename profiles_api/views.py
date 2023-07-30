5454c3511d8fd1baecb54f0427263547c7652523
swamfire <usern5949@gmail.com>
2023-07-31 03:00:53 +0530

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -102,3 +104,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name", "email", )
 
+class UserLoginApiView(ObtainAuthToken):
+    """Handle Creating user Authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
