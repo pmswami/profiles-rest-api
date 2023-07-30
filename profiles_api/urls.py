157b07f2b5c7d1db5c100c46f1abccd9c6575c35
swamfire <usern5949@gmail.com>
2023-07-31 01:12:38 +0530

    Added ProfilesViewSet

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -19,7 +19,8 @@ from profiles_api import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register("hello-viewset", views.HelloViewSet, basename="helli-viewset")
+router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
+router.register("profile", views.UserProfileViewSet)
 
 urlpatterns = [
     path("hello-view/", views.HelloApiView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -88,3 +91,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing of an object"""
         return Response({ "http_method":"DELETE"})
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes =(permissions.UpdateOwnProfile,)
+
+
