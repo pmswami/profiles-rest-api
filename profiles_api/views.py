adaa3db7c0b29dac7268d011013c6a60765ebab9
swamfire <usern5949@gmail.com>
2023-07-30 23:06:00 +0530

    Added POST, PUT ,PATCH, DELETE method to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    
+    serializer_class =serializers.HelloSerializer
+    
     def get(self, request, format=None):
         """Returns  list of PIView Features"""
         an_apiview = [
@@ -12,4 +17,29 @@ class HelloApiView(APIView):
             "Is mapped manually to URLs",
         ]
         return Response({"message":"Hello", "an_apiview":an_apiview})
+    
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializers = self.serializer_class(data=request.data)
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message':message},status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                serializers.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+    def put(self, request, pk=None):
+        """Handle updating and object"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update on objects"""
+        return Response({'method':'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """Delete Object from database"""
+        return Response({'method':'DELETE'})
     
